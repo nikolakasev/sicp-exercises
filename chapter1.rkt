ae22adea01f83b6c5150f719a241178337bc3dbd
Nikola Kasev <nikola.kasev@gmail.com>
2016-12-31 17:12:58 +0100

    completed 1.8

--- chapter1.rkt
+++ chapter1.rkt
@@ -37,3 +37,62 @@
 
 (square-of-largest-two 3 1 2)
 
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+(a-plus-abs-b 5 -10)
+
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(average 5 4)
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(sqrt 9)
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+  (else else-clause)))
+
+(new-if (= 2 3) 0 5)
+
+(define (cube x) (* x x x))
+
+(cube 2)
+
+(define (good-enough-cube? guess x)
+  (< (abs (- (cube guess) x)) 0.001))
+
+(define (improve-cube guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess))
+     3))
+
+(define (cube-iter guess x)
+  (if (good-enough-cube? guess x)
+      guess
+      (cube-iter (improve-cube guess x) x)))
+
+(define (cube-root x)
+  (cube-iter 1.0 x))
+
+(cube-root 125)
